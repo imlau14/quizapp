be0be92d1ce86c98b9451b6d4bd50717337b255d
isaac imlau <isaac.santoss@eaportal.org>
2024-06-09 21:24:24 +0000

    adição do javascript no quiz.html para trocar o tema

--- helpers/tema-helper.js
+++ helpers/tema-helper.js
@@ -1,39 +1,28 @@
 let tema = "claro"
-import { trocarTema, verificarTema } from "./helpers/tema-helper.js"
 
-const botaoTema = document.querySelector(".tema button")
-botaoTema.addEventListener("click", trocarTema)
-
-const body = document.querySelector("body")
-
-function trocarTema () {
+export function trocarTema (body, botaoTema) {
     if (localStorage.getItem("tema")) {
         tema = localStorage.getItem("tema")
     }
 
-
-
     if (tema === "claro") {
         body.classList.add("escuro")
         localStorage.setItem("tema", "escuro")
+        botaoTema.style.justifyContent = "flex-end"
     } else {
         body.classList.remove("escuro")
         localStorage.setItem("tema", "claro")
+        botaoTema.style.justifyContent = "flex-start"
     }
 }
 
-function verificarTema () {
+export function verificarTema (body, botaoTema) {
     if (localStorage.getItem("tema")) {
         tema = localStorage.getItem("tema")
     }
 
     if (tema === "escuro") {
         body.classList.add("escuro")
+        botaoTema.style.justifyContent = "flex-end"
     }
 }
-botaoTema.addEventListener("click", () => {
-    trocarTema(body, botaoTema)
-})
-
-verificarTema()
-verificarTema(body, botaoTema)
