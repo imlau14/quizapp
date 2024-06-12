5ea7403c2f7d8c30e7a7bc94c7139ed555d63739
isaac imlau <isaac.santoss@eaportal.org>
2024-06-12 01:43:10 +0000

    correçao dos erros

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -1,26 +1,36 @@
 import { verificarTema, trocarTema } from "../../helpers/tema-helper.js";
+
 const botaoTema = document.querySelector(".tema button")
 const body = document.querySelector("body")
 const assunto = localStorage.getItem("assunto")
+
 let quiz = {}
 let pontos = 0
 let pergunta = 1
 let resposta = ""
 let idInputResposta = ""
 let respostaCorretaId = ""
+
 botaoTema.addEventListener("click", () => {
     trocarTema(body, botaoTema)
 })
+
 verificarTema(body, botaoTema)
+
+
 function alterarAssunto() {
     const divIcone = document.querySelector(".assunto_icone")
     const iconeImg = document.querySelector(".assunto_icone img")
     const assuntoTitulo = document.querySelector(".assunto h1")
+
     divIcone.classList.add(assunto.toLowerCase())
     iconeImg.setAttribute("src", `../../assets/images/icon-${assunto.toLowerCase()}.svg`)
     iconeImg.setAttribute("alt", `icone de ${assunto}`)
     assuntoTitulo.innerText = assunto
 }
+
+
+
 async function buscarPerguntas() {
     const urlDados = "../../data.json"
     await fetch(urlDados).then(resposta => resposta.json()).then(dados => {
@@ -30,13 +40,17 @@ async function buscarPerguntas() {
             }
         })
     })
+
 }
+
+
+
 function montarPergunta() {
     const main = document.querySelector("main")
+
     main.innerHTML = `<section class="pergunta">
             <div>
                 <p>Questão ${pergunta} de 10</p>
-                <h2>${quiz.questions[pergunta-1].question}</h2>
                 <h2>${alterarSinais(quiz.questions[pergunta-1].question)}</h2>
             </div>
             <div class="barra_progresso">
@@ -79,25 +93,33 @@ function montarPergunta() {
             <button>Responder</button>
         </section>`
 }
+
 function alterarSinais(texto) {
     return texto.replace(/</g, "&lt;").replace(/>/g, "&gt;")
 }
+
 function guardarResposta (evento) {
     resposta = evento.target.value
     idInputResposta = evento.target.id
+
     const botaoEnviar = document.querySelector(".alternativas button")
     botaoEnviar.addEventListener("click", validarResposta)
 }
+
 function validarResposta() {
+
     const botaoEnviar = document.querySelector(".alternativas button") 
     botaoEnviar.innerText = "Próxima"
     botaoEnviar.removeEventListener("click", validarResposta)
+
     if (pergunta === 10) {
         botaoEnviar.innerText = "Finalizar"
         botaoEnviar.addEventListener("click", finalizar)
     } else {
         botaoEnviar.addEventListener("click", proximaPergunta)
     }
+
+
     if (resposta === quiz.questions[pergunta-1].answer) {
         document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "correta")
         pontos = pontos + 1
@@ -105,29 +127,38 @@ function validarResposta() {
         document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "errada")
         document.querySelector(`label[for='${respostaCorretaId}']`).setAttribute("id", "correta")
     }
+
     pergunta = pergunta + 1
 } 
+
 function finalizar() {
     localStorage.setItem("pontos", pontos)
     window.location.href = "../resultado/resultado.html"
 }
+
+
 function proximaPergunta() {
     montarPergunta()
     adicionarEventoInputs()
 }
+
 function adicionarEventoInputs() {
     const inputsResposta = document.querySelectorAll(".alternativas input")
     inputsResposta.forEach(input => {
         input.addEventListener("click", guardarResposta)
+
         if (input.value === quiz.questions[pergunta-1].answer) {
             respostaCorretaId = input.id
         }
     })
 }
+
+
 async function iniciar () {
     alterarAssunto()
     await buscarPerguntas()
     montarPergunta()
     adicionarEventoInputs()
 }
+
 iniciar()
--- pages/resultados/resultados.js
+++ pages/resultados/resultados.js
@@ -1,4 +1,5 @@
 import { verificarTema, trocarTema } from "../../helpers/tema-helper.js";
+
 const botaoTema = document.querySelector(".tema button")
 const body = document.querySelector("body")
 const assunto = localStorage.getItem("assunto")
@@ -16,16 +17,20 @@ function alterarAssunto() {
     const divIcone = document.querySelector(".assunto_icone")
     const iconeImg = document.querySelector(".assunto_icone img")
     const assuntoTitulo = document.querySelector(".assunto h1")
+
     divIcone.classList.add(assunto.toLowerCase())
     iconeImg.setAttribute("src", `../../assets/images/icon-${assunto.toLowerCase()}.svg`)
     iconeImg.setAttribute("alt", `icone de ${assunto}`)
     assuntoTitulo.innerText = assunto
 }
+
 alterarAssunto()
+
 function inserirResultado() {
     const sectionPontuacao = document.querySelector(".pontuacao")
     const divAssunto = document.querySelector(".assunto")
     const pontos = localStorage.getItem("pontos")
+
     sectionPontuacao.innerHTML = 
     ` ${divAssunto.outerHTML}
     <strong>${pontos}</strong>
